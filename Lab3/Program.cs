9617b7397bd36ed72ac7705b038a6db71bf9b9ff
Lanna <Lanna_lmb@hotmail.com>
2017-04-11 15:49:50 -0400

    Initial Commit

---
+++ Lab3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare Variables
+            int input;
+
+
+            // Input
+            Console.WriteLine("Please Enter an Integer: ");
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("Invalid Entry!  Please Enter an Integer: ");
+            }
+        }
+    }
+}
